3e560560385bfe878e05ac53e5030391fb51bdc7
AleksandraKolmakova <Amadeys88@mail.ru>
2023-05-31 20:11:39 +0300

    ITMO.WPFCsCourse2023.Lab9.Task1 Done

---
+++ ITMO.WinCsCourse2023/ITMO.WPFCsCourse2023/ITMO.WPFCsCourse2023.Lab9/ITMO.WPFCsCourse2023.Lab9.Task1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ITMO.WPFCsCourse2023.Lab9.Task1
+{
+    public class Student
+    {
+        public string StudentName { get; set; } //автоматическое свойство
+        public bool IsEnrolled { get; set; } //автоматическое свойство
+    }
+
+
+}
---
+++ ITMO.WinCsCourse2023/ITMO.WPFCsCourse2023/ITMO.WPFCsCourse2023.Lab9/ITMO.WPFCsCourse2023.Lab9.Task1/StudentList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.WPFCsCourse2023.Lab9.Task1
+{
+    public class StudentList: ObservableCollection<Student>
+    {
+
+    }
+}
