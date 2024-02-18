eee5574682f602af57c6b408f8f2e1d860ab91f0
AleksandraKolmakova <Amadeys88@mail.ru>
2024-02-18 15:58:44 +0300

    Lab2.Test1,2,3,4, are ready

--- ITMO.C++Course2024/Lab2/Lab2.Test1/Lab2.Test1.cpp
+++ ITMO.C++Course2024/Lab2/Lab2.Test1/Lab2.Test1.cpp
@@ -1,22 +1,30 @@
-﻿#include <iostream>
-#include <cmath>
+﻿#include <iostream> 
+#include <math.h> 
 using namespace std;
-
-bool isPrime(int n)
-{
-    for (int i = 2; i <= sqrt(n); i++)
-    {
-        if (n % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << isPrime(n) << endl;   
+	int n, i, flag = 1;
+	cout << "Enter a number: ";
+	cin >> n;
+
+	
+	for (i = 2; i <= sqrt(n); i++) {
+		if (n % i == 0) {
+			flag = 0;
+			break;
+		}
+	}
+
+	if (n <= 1)
+		flag = 0;
+
+	if (flag == 1) {
+		cout << n << " is a prime number";
+	}
+	else {
+		cout << n << " is not a prime number";
+	}
+
+	return 0;
 }
---
+++ ITMO.C++Course2024/Lab2/Lab2.Test4/Lab2.Test4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int score = 0; // очки
+	int shots = 0; // выстрелы
+    
+	while (score < 50){
+		int x, y;
+		cout << "Enter coordinates: ";
+		cin >> x;
+		cin >> y;
+			
+		double radius = sqrt((x*x) + (y*y));
+
+		if (radius <= 1)
+			score += 10;
+		else if (radius <= 2)
+			score += 5;
+		else score = 0;
+		shots++;
+	} 
+	
+	cout << "Your score is: "<< score << "\n" << "Your did "<< shots << " shots" << endl;
+
+	if (shots == 5)
+		cout << "Our congratulations, your level is the highest!" << endl;
+	else if (shots > 5 && score < 10)
+		cout << "Our congratulations, your level is avarige!" << endl;
+	
+	else cout << "Our congratulations, your level is begginer!" << endl;
+	
+}
